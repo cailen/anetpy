79144185413a0cf6f552a2abb1fcecc2cfab9e9b
Derek Wiedenhoeft <derek@derekdesignsorlando.com>
2016-12-14 04:12:36 -0500

    Version 0.1.0 :: The ability to pull a single Cloud Server's stats is working.

---
+++ anetpy-0.1.0/README.rst
@@ -0,0 +1,45 @@
+Atlantic.Net Cloud API Python Wrapper
+================================
+
+Inspired by [dop](https://github.com/ahmontero/dop).
+    
+    Installation
+    ============
+    
+    .. code-block:: bash
+        
+        # pip install anetpy
+    
+    Getting Started
+    ===============
+    
+    To interact with Atlantic.Net Cloud, you first need .. an Atlantic.Net Cloud account with 
+    valid API keys.
+    
+    Keys can be set either as Env variables, or within the code.
+        
+    .. code-block:: bash
+        
+        # export ANET_PUBLIC_KEY='public_key'
+        # export ANET_PRIVATE_KEY='private_key'
+    
+    .. code-block:: pycon
+    
+        >>> from anetpy.manager import AnetManager
+        >>> anet = AnetManager('public_key', 'private_key')
+
+Methods
+=======
+
+The methods of the AnetManager are self explanatory; ex.
+        
+        .. code-block:: pycon
+        
+            >>> anet.all_active_cloudservers()
+            >>> anet.show_cloudserver('12345')
+            >>> anet.destroy_cloudserver('12345')
+            >>> anet.all_regions()
+            >>> anet.all_images()
+            >>> anet.all_ssh_keys()
+            >>> anet.plans()
+            >>> anet.new_cloudserver('new_cloudserver', 66, 1601, 1)
---
+++ anetpy-0.1.0/anetpy/manager.py
@@ -0,0 +1,200 @@
+#!/usr/bin/env python
+#coding: utf-8
+"""
+This module simply sends request to the Atlantic.Net API,
+and returns their response as a dict.
+"""
+
+import requests
+import json as json_module
+from six import wraps
+import time
+import uuid
+import urllib
+import hmac
+import hashlib
+import base64
+from collections import OrderedDict
+
+API_ENDPOINT = 'https://cloudapi.atlantic.net/'
+
+class AnetError(RuntimeError):
+    pass
+
+class AnetManager(object):
+
+    def __init__(self, public_key, private_key, api_version="2010-12-30"):
+        self.api_endpoint = API_ENDPOINT
+        self.public_key = public_key
+        self.private_key = private_key
+
+    def all_active_cloudservers(self):
+        params = {
+            'Action': 'list-instances',
+        }
+        json_resp = self.request(params)
+        
+        return json_resp['list-instancesresponse']['instancesSet']
+
+    def new_cloudserver(self, servername, planname, imageid, vm_location,
+            key_id=None, enablebackup=False):
+
+        params = {
+	    'Action': 'run-instance',
+            'servername': str(servername),
+            'planname': str(planname),
+            'imageid': str(imageid),
+            'vm_location': str(vm_location),
+            'key_id': str(key_id),
+            'enablebackup': str(enablebackup).lower()
+        }
+
+        json = self.request(params)
+        return json['run-instanceresponse']['instancesSet']
+
+    def show_cloudserver(self, instanceid):
+        params = {
+	    'Action': 'describe-instance',
+	    'instanceid': instanceid,
+	}
+        json = self.request(params)
+        return json['describe-instanceresponse']['instanceSet']
+
+    def reboot_cloudserver(self, instanceid):
+        params = {
+	    'Action':'reboot-instance',
+	    'instanceid': instanceid, 
+	    'reboottype': 'soft'
+	}
+        json = self.request(params)
+        json.pop('status', None)
+        return json['reboot-instanceresponse']['return']
+
+    def power_cycle_cloudserver(self, instanceid):
+        params = {
+	    'Action': 'reboot-instance',
+	    'instanceid': instanceid, 
+	    'reboottype': 'hard'
+	}
+        json = self.request(params)
+        json.pop('status', None)
+        return json['reboot-instanceresponse']['return']
+
+    def destroy_cloudserver(self, instanceids, scrub_data=True):
+        params = {
+	    'Action': 'terminate-instance',
+	}
+        count = 0
+        for instanceid in instanceids:
+            count+=1
+            params['instanceid_'+ count] = instanceid
+
+        json = self.request(params)
+        json.pop('status', None)
+        return json['terminate-instanceresponse']['instancesSet']
+
+    def populate_cloudserver_ips(self, cloudserver):
+        cloudserver[u'ip_address'] = ''
+        for networkIndex in range(len(cloudserver['networks']['v4'])):
+            network = cloudserver['networks']['v4'][networkIndex]
+            if network['type'] == 'public':
+                cloudserver[u'ip_address'] = network['ip_address']
+            if network['type'] == 'private':
+                cloudserver[u'private_ip_address'] = network['ip_address']
+
+#images==========================================
+    def all_images(self):
+        params = {
+			'Action': 'describe-image',
+		}
+        json = self.request(params)
+        return json['describe-imageresponse']['imagesset']
+
+    def show_image(self, imageid):
+        params = {
+			'Action': 'describe-image',
+			'imageid': imageid
+		}
+        json = self.request(params)
+        return json['describe-imageresponse']['imagesset']
+
+#ssh_keys=========================================
+    def all_ssh_keys(self):
+        params = {
+			'Action': 'list-sshkeys',
+		}
+        json = self.request('Action=list-sshkeys')
+        return json['list-sshkeysresponse']['KeysSet']
+
+#plans============================================
+    def plans(self):
+        params = {
+			'Action': 'describe-plan',
+		}
+        json = self.request(params)
+        return json['describe-planresponse']['plans']
+
+#low_level========================================
+    def request(self, params={}, method='GET'):
+        random_guid = str(uuid.uuid4())
+        time_since_epoch = int(time.time())
+        string_to_sign = str(time_since_epoch) + str(random_guid)
+        url = API_ENDPOINT
+        signature = self.signature_request(string_to_sign, self.private_key)
+
+        orderparams = OrderedDict()
+        orderparams['Action'] = str(params['Action'])
+        orderparams['Format'] = "json"
+        orderparams['Version'] = "2010-12-30"
+        orderparams['ACSAccessKeyId'] = str(self.public_key)
+        orderparams['Timestamp'] = str(time_since_epoch)
+        orderparams['Rndguid'] = str(random_guid)
+        orderparams['Signature'] = str(signature)
+
+        resp = self.request_v1(url, orderparams, method=method)
+
+        return resp
+
+    def signature_request(self, string_to_sign, private_key):
+        signature=hmac.new(self.private_key, string_to_sign,hashlib.sha256).digest()
+        signature=base64.encodestring(signature)
+        signature = signature.rstrip()
+
+        return signature
+
+    def request_v1(self, url, params={}, method='GET'):
+        params=urllib.urlencode(params)
+        try:
+            resp = requests.get(url, params=params, timeout=60)
+            json_resp = resp.json()
+        except ValueError:  # requests.models.json.JSONDecodeError
+            raise ValueError("The API server doesn't respond with a valid json")
+        except requests.RequestException as e:  # errors from requests
+            raise RuntimeError(e, resp)
+
+        if resp.status_code != requests.codes.ok:
+            if json_resp:
+                if 'error_message' in json_resp:
+                    raise AnetError(json_resp['message'])
+                elif 'message' in json_resp:
+                    raise AnetError(json_resp['message'])
+            # The JSON reponse is bad, so raise an exception with the HTTP status
+            resp.raise_for_status()
+        if str(json_resp.get('error')).lower() != 'none':
+            raise AnetError(json_resp['error']['message'])
+
+        return json_resp
+
+if __name__ == '__main__':
+    import os
+    #public_key = os.environ['ANET_PRIVATE_KEY']
+    #private_key = os.environ['ANET_PUBLIC_KEY']
+    public_key = 'ATL8f59337f60fb45e4ff600c38e62ab540'
+    private_key = '66f002a2b6c5d742a9ce6d6e4de333534c73b128'
+    anet = AnetManager(public_key, private_key,"2010-12-30")
+    testvar = anet.all_active_cloudservers()
+    import sys
+    #fname = sys.argv[1]
+    import pprint
+    # size_id: 66, image_id: 1601, region_id: 1
+    #pprint.pprint(getattr(anet, fname)(*sys.argv[2:]))
